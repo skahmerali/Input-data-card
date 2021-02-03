aa6da9599fc0ae4f40e89e99522f084f0873c2e7
ahmed <raza26032@gmail.com>
2021-02-03 17:02:06 +0500

    learning point

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function App() {
+  const [Data, setData] = useState([])
+
+  const posts = (e) => {
+    e.preventDefault()
+    var name = document.getElementById("name").value;
+    var text = document.getElementById("text").value;
+    var image = document.getElementById("imageUrl").value;
+    var date = new Date().toLocaleDateString()
+    console.log()
+    let newData = {
+      name: name,
+      text: text,
+      image: image,
+      date: date
+    }
+
+  
+
+    // console.log(newData)
+    setData((previousDefault) => {
+      return previousDefault.concat([newData]);
+    })
+  }
+  return (
+    <div>
+      <h1>HeLLo PakisTanI</h1>
+      <form onSubmit={posts}>
+        <p>Name : </p>
+
+        <input type='text' id="name" />
+        <p>Writte SomeText</p>
+        <input type='text' id="text" />
+        <p>submit ImgUrl</p>
+        <input type='text' id="imageUrl" />
+        <button type='submit'>Post</button>
+      </form>
+
+
+
+      {Data.map((EachPost, i) => {
+        // console.log(EachPost)
+        return (
+
+          <div key={i} className="post">
+            <div className="userInfo">
+              <div className="profilePic">
+                <img className="userPic" src={EachPost.image} />
+              </div>
+              <div className="userName">
+                <h2>{EachPost.name}</h2>
+                <p>{EachPost.date}</p>
+              </div>
+
+            </div>
+            <div className="psotText">
+              <img className="image" src={EachPost.image} />
+            </div>
+          </div>
+        )
+
+      })}
+
+
+      
+
+    </div>
+
+
+  )
+
+
+}
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
